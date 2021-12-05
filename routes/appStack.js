ac92a1a1d9f930d9a6b5417a344a306b91eae098
JamesMorey <jamesrmorey@gmail.com>
2021-12-05 11:35:45 +0000

    Bottom Tabs initial styling

--- routes/appStack.js
+++ routes/appStack.js
@@ -4,14 +4,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import TrailsStack from "./trailsStack";
 import BadgesStack from "./badgesStack";
 
+//Icons
+import { Fontisto, SimpleLineIcons } from '@expo/vector-icons'; 
+
 const Tab = createBottomTabNavigator();
 
 export default function AppStack() {
   return (
     <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="Trails" component={TrailsStack}/>
-            <Tab.Screen name="Badges" component={BadgesStack}/>
+        <Tab.Navigator screenOptions={{
+            tabBarStyle: {
+                backgroundColor: '#fff',
+                height: 100,
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            tabBarLabelStyle: {
+                display: "none",
+            },
+            tabBarActiveTintColor: '#fa7002',
+            tabBarInactiveTintColor: '#A9A9A9'
+        }}>
+            <Tab.Screen name="Trails" component={TrailsStack} options={{
+                tabBarIcon: ({ color, size }) => (<Fontisto name="map" size={35} color={color}/>)
+            }}/>
+            <Tab.Screen name="Badges" component={BadgesStack} options={{
+                tabBarIcon: ({ color, size }) => (<SimpleLineIcons name="badge" size={35} color={color} />)
+            }}/>
         </Tab.Navigator>
     </NavigationContainer>
   );
